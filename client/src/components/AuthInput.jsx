5903de81041563ea6095290a8f13df0bb86d9a9c
eguono imonieroh <imonieroheguono@gmail.com>
2025-09-16 16:11:40 +0100

    fixes:sign-in page

--- client/src/components/AuthInput.jsx
+++ client/src/components/AuthInput.jsx
@@ -11,7 +11,7 @@ const AuthInput = ({label,htmlFor,type = "text",placeholder,onChange,value,  req
     </div>
     <div>
 
-    <input type={type}   required = {required} id={htmlFor} value={value} onChange={onChange} placeholder={placeholder} className='border-[#2E4052] border-[1px] w-full h-[56px] ps-4 bg-[#1E2936]  placeholder:text-[#98A2B3] placeholder:font-[400]  rounded-md mb-3'/>
+    <input type={type}   required = {required} id={htmlFor} value={value} onChange={onChange} placeholder={placeholder} className='border-[#2E4052] border-[1px] w-full h-[56px] ps-4 bg-[#1E2936] text-white  placeholder:text-[#98A2B3] placeholder:font-[400]  rounded-md mb-3'/>
     </div>
     </>
   )
